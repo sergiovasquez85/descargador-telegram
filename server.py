b1d87a46c0baa3d23f2ecb6020a592ba7df4be0e
sergiovasquez85 <104742162+sergiovasquez85@users.noreply.github.com>
2025-02-23 00:18:53 -0500

    Update server.py

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def webhook():
     data = request.get_json()
     if data:
         update = Update.de_json(data, telegram_app.bot)
-        telegram_app.create_task(telegram_app.process_update(update))
+         asyncio.run(telegram_app.process_update(update))
     return "ok", 200
 
 @flask_app.route("/", methods=["GET"])
